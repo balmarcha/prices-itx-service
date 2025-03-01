859795a79bac9878b3dd9c63d9d9609e9be0744b
Begoña Almarcha Guillén <balmarchaguillen@gmail.com>
2025-03-01 18:06:18 +0100

    feat(structure): Se crea la estructura inicial del proyecto

---
+++ infrastructure/src/main/java/es/bxg/ps/PricesItxApplication.java
@@ -0,0 +1,13 @@
+package es.bxg.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesItxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PricesItxApplication.class, args);
+  }
+
+}
